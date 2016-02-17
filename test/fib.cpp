eae3a703013fd70a36bd565d4f85149e2b377048
Aksenov <aksenov.vitaly@gmail.com>
2016-02-17 17:21:25 +0100

    change

--- test/fib.cpp
+++ test/fib.cpp
@@ -1,6 +1,6 @@
 /*!
  * \file fib.cpp
- * \brief Benchmarking script for parallel sorting algorithms
+ * \brief Fibonacci example
  * \date 2015
  * \copyright COPYRIGHT (c) 2015 Umut Acar, Arthur Chargueraud, and
  * Michael Rainey. All rights reserved.
