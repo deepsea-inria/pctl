cc5277b178e25e08c19d670dfbfbbfe23c9c09bf
Aksenov <aksenov.vitaly@gmail.com>
2016-03-15 11:13:46 +0100

    scan bug fix

--- test/utils/reducers.hpp
+++ test/utils/reducers.hpp
@@ -6,7 +6,8 @@
 #define _PARUTILS_REDUCERS_H_
 
 namespace parutils {
-namespace reducers {
+namespace array {
+namespace utils {
 
 #define BLOCK_SIZE 1024
 
@@ -68,7 +69,7 @@ Item reduce(Array<Item>& items, const Item& zero, const Multiply_fct& multiplica
   if (blocks == 1) {
     return reduce_serial(items, 0, items.size(), zero, multiplication);
   }
-  array::array<Item> tmp_array(2 * blocks);
+  array<Item> tmp_array(2 * blocks);
   return reduce(items, 0, items.size(), tmp_array, 0, zero, multiplication);
 }
 
@@ -80,16 +81,19 @@ template <template <class Item> class Array, template <class Item> class ResultA
 void scan_exclusive(Array<Item>& items, size_t l, size_t r, ResultArray<Item>& result, size_t result_offset, TmpArray<Item>& tmp_array, size_t tmp_offset, const Item& zero, const Multiply_fct& multiplication) {
   size_t blocks = (r - l + BLOCK_SIZE - 1) / BLOCK_SIZE;
   if (blocks == 1) {
-    scan_exclusive_serial(items, 0, r - l, result, 0, zero, multiplication);
+    scan_exclusive_serial(items, l, r, result, result_offset, zero, multiplication);
     return;
   }
 
   pasl::pctl::parallel_for((size_t)0, blocks, [&] (size_t i) {
     tmp_array.at(tmp_offset + i) = reduce_serial(items, l + i * BLOCK_SIZE, l + std::min((i + 1) * BLOCK_SIZE, r - l), zero, multiplication);
 //    if (blocks == 2)
+//      std::cerr << tmp_array.at(tmp_offset + i) << std::endl;
   });
   scan_exclusive(tmp_array, tmp_offset, tmp_offset + blocks, tmp_array, tmp_offset, tmp_array, tmp_offset + blocks, zero, multiplication);
   pasl::pctl::parallel_for((size_t)0, blocks, [&] (size_t i) {
+//    if (blocks == 2)
+//      std::cerr << tmp_array.at(tmp_offset + i) << std::endl;
     scan_exclusive_serial(items, l + i * BLOCK_SIZE, l + std::min((i + 1) * BLOCK_SIZE, r - l), result, result_offset + i * BLOCK_SIZE, i == 0 ? zero : tmp_array.at(tmp_offset + i - 1), multiplication);
   });
 }
@@ -111,20 +115,20 @@ void scan_exclusive_no_tmp(Array<Item>& items, ResultArray<Item>& result, const 
     scan_exclusive_serial(items, 0, items.size(), result, 0, zero, multiplication);
     return;
   }
-  array::array<Item> tmp_array(2 * blocks);
+  array<Item> tmp_array(2 * blocks);
   scan_exclusive(items, 0, items.size(), result, 0, tmp_array, 0, zero, multiplication);
 }
 
 template <template <class Item> class Array, template <class Item> class TmpArray, class Item, class Multiply_fct>
-array::array<Item> scan_exclusive_no_result(Array<Item>& items, TmpArray<Item>& tmp_array, const Item& zero, const Multiply_fct& multiplication) {
-  array::array<Item> result(items.size());
+array<Item> scan_exclusive_no_result(Array<Item>& items, TmpArray<Item>& tmp_array, const Item& zero, const Multiply_fct& multiplication) {
+  array<Item> result(items.size());
   scan_exclusive(items, 0, items.size(), result, 0, tmp_array, 0, zero, multiplication);
   return result;
 }
 
 template <template <class Item> class Array, class Item, class Multiply_fct>
-array::array<Item> scan_exclusive(Array<Item>& items, const Item& zero, const Multiply_fct& multiplication) {
-  array::array<Item> result(items.size());
+array<Item> scan_exclusive(Array<Item>& items, const Item& zero, const Multiply_fct& multiplication) {
+  array<Item> result(items.size());
   scan_exclusive_no_tmp(items, result, zero, multiplication);
   return result;
 }
@@ -146,26 +150,27 @@ void scan_inclusive_no_tmp(Array<Item>& items, ResultArray<Item>& result, const 
     scan_exclusive_serial(items, 0, items.size(), result, 1, zero, multiplication);
     return;
   }
-  array::array<Item> tmp_array(2 * blocks);
+  array<Item> tmp_array(2 * blocks);
   result[0] = zero;
   scan_exclusive(items, 0, items.size(), result, 1, tmp_array, 0, zero, multiplication);
 }
 
 template <template <class Item> class Array, template <class Item> class TmpArray, class Item, class Multiply_fct>
-array::array<Item> scan_inclusive_no_result(Array<Item>& items, TmpArray<Item>& tmp_array, const Item& zero, const Multiply_fct& multiplication) {
-  array::array<Item> result(items.size() + 1);
+array<Item> scan_inclusive_no_result(Array<Item>& items, TmpArray<Item>& tmp_array, const Item& zero, const Multiply_fct& multiplication) {
+  array<Item> result(items.size() + 1);
   result[0] = zero;
   scan_exclusive(items, 0, items.size(), result, 1, tmp_array, 0, zero, multiplication);
   return result;
 }
 
 template <template <class Item> class Array, class Item, class Multiply_fct>
-array::array<Item> scan_inclusive(Array<Item>& items, const Item& zero, const Multiply_fct& multiplication) {
-  array::array<Item> result(items.size() + 1);
+array<Item> scan_inclusive(Array<Item>& items, const Item& zero, const Multiply_fct& multiplication) {
+  array<Item> result(items.size() + 1);
   scan_inclusive_no_tmp(items, result, zero, multiplication);
   return result;
 }
 
+} //end namespace
 } //end namespace
 } //end namespace
 
