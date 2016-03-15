5cf353d70574f26b934e032d88ca76f1e651c1ca
Aksenov <aksenov.vitaly@gmail.com>
2016-03-15 13:24:31 +0100

    documentation for reducers.hpp

--- test/utils/reducers.hpp
+++ test/utils/reducers.hpp
@@ -1,3 +1,12 @@
+/*!
+  \file
+  \brief File contains implementations of two main functions on array (reduce, scan) with
+  different arguments.
+
+  In this file reduce and scan with different arguments are provided. Also,
+  different weighted versions of reduce functions are provided.
+*/
+
 #include <algorithm>
 #include "ploop.hpp"
 #include <iostream>
@@ -12,8 +21,8 @@ namespace utils {
 #define BLOCK_SIZE 1024
 
 template <template <class Item> class Array, class Item, class Multiply_fct>
-Item reduce_serial(Array<Item>& items, size_t l, size_t r, const Item& zero, const Multiply_fct& multiplication) {
-  Item result = zero;
+Item reduce_serial(Array<Item>& items, size_t l, size_t r, const Item& identity, const Multiply_fct& multiplication) {
+  Item result = identity;
   for (int i = l; i < r; ++i) {
     result = multiplication(result, items.at(i));
   }
@@ -21,8 +30,8 @@ Item reduce_serial(Array<Item>& items, size_t l, size_t r, const Item& zero, con
 }
 
 template <template <class Item> class Array, template <class Item> class ResultArray, class Item, class Multiply_fct>
-void scan_exclusive_serial(Array<Item>& items, size_t l, size_t r, ResultArray<Item>& result, size_t result_offset, const Item& zero, const Multiply_fct& multiplication) {
-  Item current = zero;
+void scan_exclusive_serial(Array<Item>& items, size_t l, size_t r, ResultArray<Item>& result, size_t result_offset, const Item& identity, const Multiply_fct& multiplication) {
+  Item current = identity;
   for (int i = l; i < r; i++) {
 //
     current = result.at(result_offset + i - l) = multiplication(current, items.at(i));  
@@ -30,106 +39,176 @@ void scan_exclusive_serial(Array<Item>& items, size_t l, size_t r, ResultArray<I
 }
 
 template <template <class Item> class Array, class Item, class Multiply_fct>
-void scan_exclusive_serial(Array<Item>& items, size_t l, size_t r, const Item& zero, const Multiply_fct& multiplication) {
+void scan_exclusive_serial(Array<Item>& items, size_t l, size_t r, const Item& identity, const Multiply_fct& multiplication) {
   scan_exclusive_serial(items, l, r, items, l, multiplication);
 }
 
 template <template <class Item> class Array, template <class Item> class ResultArray, class Item, class Multiply_fct>
-void scan_inclusive_serial(Array<Item>& items, size_t l, size_t r, ResultArray<Item>& result, size_t result_offset, const Item& zero, const Multiply_fct& multiplication) {
-  result.at(result_offset) = zero;
+void scan_inclusive_serial(Array<Item>& items, size_t l, size_t r, ResultArray<Item>& result, size_t result_offset, const Item& identity, const Multiply_fct& multiplication) {
+  result.at(result_offset) = identity;
   scan_exclusive_serial(items, result, l + 1, r, result_offset + 1, multiplication);
 }
 
 /***
 Reduce functions.
 ***/
-
+/*!
+  Calculates the value of given multiplication function on elements of the given array in certain range using given temporary array.
+  \param items array of elements
+  \param l start position of range
+  \param r end position of range (exclusive)
+  \param tmp_array temporary array to hold the temporary work (should be at least approximately 2 * (r - l) / BLOCK_SIZE length)
+  \param tmp_offset starting from this offset the values of tmp_array could be used
+  \param identity identity element for mulitplication function
+  \param multiplication multiplication function
+*/
 template <template <class Item> class Array, template <class Item> class TmpArray, class Item, class Multiply_fct>
-Item reduce(Array<Item>& items, size_t l, size_t r, TmpArray<Item>& tmp_array, size_t tmp_offset, const Item& zero, const Multiply_fct& multiplication) {
-  Item result = zero;
+Item reduce(Array<Item>& items, size_t l, size_t r, TmpArray<Item>& tmp_array, size_t tmp_offset, const Item& identity, const Multiply_fct& multiplication) {
+  Item result = identity;
   size_t blocks = (r - l + BLOCK_SIZE - 1) / BLOCK_SIZE;
   if (blocks == 1) {
-    return reduce_serial(items, l, r, zero, multiplication);
+    return reduce_serial(items, l, r, identity, multiplication);
   }
   pasl::pctl::parallel_for((size_t)0, blocks, [&] (size_t i) {
-    tmp_array.at(i + tmp_offset) = reduce_serial(items, l + i * BLOCK_SIZE, l + std::min((i + 1) * BLOCK_SIZE, r - l), zero, multiplication);
+    tmp_array.at(i + tmp_offset) = reduce_serial(items, l + i * BLOCK_SIZE, l + std::min((i + 1) * BLOCK_SIZE, r - l), identity, multiplication);
   });
 
-  return reduce(tmp_array, tmp_offset, tmp_offset + blocks, tmp_array, tmp_offset + blocks, zero, multiplication);
+  return reduce(tmp_array, tmp_offset, tmp_offset + blocks, tmp_array, tmp_offset + blocks, identity, multiplication);
 }
 
+/*!
+  Calculates the value of given multiplication function on elements of the given array using given temporary array.
+  \param items array of elements
+  \param tmp_array temporary array to hold the temporary work (should be at least approximately 2 * items.size() / BLOCK_SIZE length)
+  \param identity unary element for mulitplication function
+  \param multiplication multiplication function
+*/
 template <template <class Item> class Array, template <class Item> class TmpArray, class Item, class Multiply_fct>
-Item reduce(Array<Item>& items, TmpArray<Item>& tmp_array, const Item& zero, const Multiply_fct& multiplication) {
-  return reduce(items, items.size(), tmp_array, 0, zero, multiplication);
+Item reduce(Array<Item>& items, TmpArray<Item>& tmp_array, const Item& identity, const Multiply_fct& multiplication) {
+  return reduce(items, items.size(), tmp_array, 0, identity, multiplication);
 }
 
+/*!
+  Calculates the value of given multiplication function on elements of the given array.
+  Uses additional 2 * items.size() / BLOCK_SIZE length temporary array.
+  \param items array of elements
+  \param identity identity element for mulitplication function
+  \param multiplication multiplication function
+*/
 template <template <class Item> class Array, class Item, class Multiply_fct>
-Item reduce(Array<Item>& items, const Item& zero, const Multiply_fct& multiplication) {
+Item reduce(Array<Item>& items, const Item& identity, const Multiply_fct& multiplication) {
   size_t blocks = (items.size() + BLOCK_SIZE - 1) / BLOCK_SIZE;
   if (blocks == 1) {
-    return reduce_serial(items, 0, items.size(), zero, multiplication);
+    return reduce_serial(items, 0, items.size(), identity, multiplication);
   }
   array<Item> tmp_array(2 * blocks);
-  return reduce(items, 0, items.size(), tmp_array, 0, zero, multiplication);
+  return reduce(items, 0, items.size(), tmp_array, 0, identity, multiplication);
 }
 
 /***
 Scan exclusive functions.
 ***/
 
+/*!
+  Calculates the exclusive scan array (i.e. array of prefix sums without identity element first) of given multiplication function on elements
+  of the given array in certain range using given temporary array and into specified array.
+  \param items array of elements
+  \param l start position of range
+  \param r end position of range (exclusive)
+  \param result array to contain result of scan
+  \param result_offset position in result array from where start to write
+  \param tmp_array temporary array to hold the temporary work (should be at least approximately 2 * (r - l) / BLOCK_SIZE length)
+  \param tmp_offset starting from this offset the values of tmp_array could be used
+  \param identity identity element for mulitplication function
+  \param multiplication multiplication function
+*/
 template <template <class Item> class Array, template <class Item> class ResultArray, template <class Item> class TmpArray, class Item, class Multiply_fct>
-void scan_exclusive(Array<Item>& items, size_t l, size_t r, ResultArray<Item>& result, size_t result_offset, TmpArray<Item>& tmp_array, size_t tmp_offset, const Item& zero, const Multiply_fct& multiplication) {
+void scan_exclusive(Array<Item>& items, size_t l, size_t r, ResultArray<Item>& result, size_t result_offset, TmpArray<Item>& tmp_array, size_t tmp_offset, const Item& identity, const Multiply_fct& multiplication) {
   size_t blocks = (r - l + BLOCK_SIZE - 1) / BLOCK_SIZE;
   if (blocks == 1) {
-    scan_exclusive_serial(items, l, r, result, result_offset, zero, multiplication);
+    scan_exclusive_serial(items, l, r, result, result_offset, identity, multiplication);
     return;
   }
 
   pasl::pctl::parallel_for((size_t)0, blocks, [&] (size_t i) {
-    tmp_array.at(tmp_offset + i) = reduce_serial(items, l + i * BLOCK_SIZE, l + std::min((i + 1) * BLOCK_SIZE, r - l), zero, multiplication);
+    tmp_array.at(tmp_offset + i) = reduce_serial(items, l + i * BLOCK_SIZE, l + std::min((i + 1) * BLOCK_SIZE, r - l), identity, multiplication);
 //    if (blocks == 2)
 //      std::cerr << tmp_array.at(tmp_offset + i) << std::endl;
   });
-  scan_exclusive(tmp_array, tmp_offset, tmp_offset + blocks, tmp_array, tmp_offset, tmp_array, tmp_offset + blocks, zero, multiplication);
+  scan_exclusive(tmp_array, tmp_offset, tmp_offset + blocks, tmp_array, tmp_offset, tmp_array, tmp_offset + blocks, identity, multiplication);
   pasl::pctl::parallel_for((size_t)0, blocks, [&] (size_t i) {
 //    if (blocks == 2)
 //      std::cerr << tmp_array.at(tmp_offset + i) << std::endl;
-    scan_exclusive_serial(items, l + i * BLOCK_SIZE, l + std::min((i + 1) * BLOCK_SIZE, r - l), result, result_offset + i * BLOCK_SIZE, i == 0 ? zero : tmp_array.at(tmp_offset + i - 1), multiplication);
+    scan_exclusive_serial(items, l + i * BLOCK_SIZE, l + std::min((i + 1) * BLOCK_SIZE, r - l), result, result_offset + i * BLOCK_SIZE, i == 0 ? identity : tmp_array.at(tmp_offset + i - 1), multiplication);
   });
 }
 
+/*!
+  Calculates the exclusive scan array (i.e. array of prefix sums without identity element first) of given multiplication function on elements
+  of the given array using given temporary array and into specified array.
+  \param items array of elements
+  \param result array to contain result of scan
+  \param tmp_array temporary array to hold the temporary work (should be at least approximately 2 * items.size() / BLOCK_SIZE length)
+  \param identity identity element for mulitplication function
+  \param multiplication multiplication function
+*/
 template <template <class Item> class Array, template <class Item> class ResultArray, template <class Item> class TmpArray, class Item, class Multiply_fct>
-void scan_exclusive(Array<Item>& items, ResultArray<Item>& result, TmpArray<Item>& tmp_array, const Item& zero, const Multiply_fct& multiplication) {
+void scan_exclusive(Array<Item>& items, ResultArray<Item>& result, TmpArray<Item>& tmp_array, const Item& identity, const Multiply_fct& multiplication) {
   size_t blocks = (items.size() + BLOCK_SIZE - 1) / BLOCK_SIZE;
   if (blocks == 1) {
-    scan_exclusive_serial(items, result, 0, items.size(), 0, zero, multiplication);
+    scan_exclusive_serial(items, result, 0, items.size(), 0, identity, multiplication);
     return;
   }
-  scan_exclusive(items, 0, items.size(), result, 0, tmp_array, 0, zero, multiplication);
+  scan_exclusive(items, 0, items.size(), result, 0, tmp_array, 0, identity, multiplication);
 }
 
+/*!
+  Calculates the exclusive scan array (i.e. array of prefix sums without identity element first) of given multiplication function on elements
+  of the given array into specified array. Uses additional 2 * items.size() / BLOCK_SIZE length temporary array.
+  \param items array of elements
+  \param result array to contain result of scan
+  \param identity identity element for mulitplication function
+  \param multiplication multiplication function
+*/
 template <template <class Item> class Array, template <class Item> class ResultArray, class Item, class Multiply_fct>
-void scan_exclusive_no_tmp(Array<Item>& items, ResultArray<Item>& result, const Item& zero, const Multiply_fct& multiplication) {
+void scan_exclusive_no_tmp(Array<Item>& items, ResultArray<Item>& result, const Item& identity, const Multiply_fct& multiplication) {
   size_t blocks = (items.size() + BLOCK_SIZE - 1) / BLOCK_SIZE;
   if (blocks == 1) {
-    scan_exclusive_serial(items, 0, items.size(), result, 0, zero, multiplication);
+    scan_exclusive_serial(items, 0, items.size(), result, 0, identity, multiplication);
     return;
   }
   array<Item> tmp_array(2 * blocks);
-  scan_exclusive(items, 0, items.size(), result, 0, tmp_array, 0, zero, multiplication);
+  scan_exclusive(items, 0, items.size(), result, 0, tmp_array, 0, identity, multiplication);
 }
 
+/*!
+  Calculates the exclusive scan array (i.e. array of prefix sums without identity element first) of given multiplication function on elements
+  of the given array using given temporary array.
+  \param items array of elements
+  \param tmp_array temporary array to hold the temporary work (should be at least approximately 2 * items.size() / BLOCK_SIZE length)
+  \param identity identity element for mulitplication function
+  \param multiplication multiplication function
+  \return array with results of scan
+*/
 template <template <class Item> class Array, template <class Item> class TmpArray, class Item, class Multiply_fct>
-array<Item> scan_exclusive_no_result(Array<Item>& items, TmpArray<Item>& tmp_array, const Item& zero, const Multiply_fct& multiplication) {
+array<Item> scan_exclusive_no_result(Array<Item>& items, TmpArray<Item>& tmp_array, const Item& identity, const Multiply_fct& multiplication) {
   array<Item> result(items.size());
-  scan_exclusive(items, 0, items.size(), result, 0, tmp_array, 0, zero, multiplication);
+  scan_exclusive(items, 0, items.size(), result, 0, tmp_array, 0, identity, multiplication);
   return result;
 }
 
+/*!
+  Calculates the exclusive scan array (i.e. array of prefix sums without identity element first) of given multiplication function on elements
+  of the given array. Uses additional 2 * items.size() / BLOCK_SIZE length temporary array.
+  \param items array of elements
+  \param identity identity element for mulitplication function
+  \param multiplication multiplication function
+  \return array with results of scan
+*/
 template <template <class Item> class Array, class Item, class Multiply_fct>
-array<Item> scan_exclusive(Array<Item>& items, const Item& zero, const Multiply_fct& multiplication) {
+array<Item> scan_exclusive(Array<Item>& items, const Item& identity, const Multiply_fct& multiplication) {
   array<Item> result(items.size());
-  scan_exclusive_no_tmp(items, result, zero, multiplication);
+  scan_exclusive_no_tmp(items, result, identity, multiplication);
   return result;
 }
 
@@ -137,36 +216,70 @@ array<Item> scan_exclusive(Array<Item>& items, const Item& zero, const Multiply_
 Scan inclusive functions.
 ***/
 
+/*!
+  Calculates the inclusive scan array (i.e. array of prefix sums with identity element first) of given multiplication function on elements
+  of the given array using given temporary array and into specified array.
+  \param items array of elements
+  \param result array to contain result of scan
+  \param tmp_array temporary array to hold the temporary work (should be at least approximately 2 * items.size() / BLOCK_SIZE length)
+  \param identity identity element for mulitplication function
+  \param multiplication multiplication function
+*/
 template <template <class Item> class Array, template <class Item> class ResultArray, template <class Item> class TmpArray, class Item, class Multiply_fct>
-void scan_inclusive(Array<Item>& items, ResultArray<Item>& result, TmpArray<Item>& tmp_array, const Item& zero, const Multiply_fct& multiplication) {
-  result[0] = zero;
-  scan_exclusive(items, 0, items.size(), result, 1, tmp_array, 0, zero, multiplication);
+void scan_inclusive(Array<Item>& items, ResultArray<Item>& result, TmpArray<Item>& tmp_array, const Item& identity, const Multiply_fct& multiplication) {
+  result[0] = identity;
+  scan_exclusive(items, 0, items.size(), result, 1, tmp_array, 0, identity, multiplication);
 }
 
+/*!
+  Calculates the inclusive scan array (i.e. array of prefix sums with identity element first) of given multiplication function on elements
+  of the given array into specified array. Uses additional 2 * items.size() / BLOCK_SIZE length temporary array.
+  \param items array of elements
+  \param result array to contain result of scan
+  \param identity identity element for mulitplication function
+  \param multiplication multiplication function
+*/
 template <template <class Item> class Array, template <class Item> class ResultArray, class Item, class Multiply_fct>
-void scan_inclusive_no_tmp(Array<Item>& items, ResultArray<Item>& result, const Item& zero, const Multiply_fct& multiplication) {
+void scan_inclusive_no_tmp(Array<Item>& items, ResultArray<Item>& result, const Item& identity, const Multiply_fct& multiplication) {
   size_t blocks = (items.size() + BLOCK_SIZE - 1) / BLOCK_SIZE;
   if (blocks == 1) {
-    scan_exclusive_serial(items, 0, items.size(), result, 1, zero, multiplication);
+    scan_exclusive_serial(items, 0, items.size(), result, 1, identity, multiplication);
     return;
   }
   array<Item> tmp_array(2 * blocks);
-  result[0] = zero;
-  scan_exclusive(items, 0, items.size(), result, 1, tmp_array, 0, zero, multiplication);
+  result[0] = identity;
+  scan_exclusive(items, 0, items.size(), result, 1, tmp_array, 0, identity, multiplication);
 }
 
+/*!
+  Calculates the inclusive scan array (i.e. array of prefix sums with identity element first) of given multiplication function on elements
+  of the given array using given temporary array.
+  \param items array of elements
+  \param tmp_array temporary array to hold the temporary work (should be at least approximately 2 * items.size() / BLOCK_SIZE length)
+  \param identity identity element for mulitplication function
+  \param multiplication multiplication function
+  \return array with results of scan
+*/
 template <template <class Item> class Array, template <class Item> class TmpArray, class Item, class Multiply_fct>
-array<Item> scan_inclusive_no_result(Array<Item>& items, TmpArray<Item>& tmp_array, const Item& zero, const Multiply_fct& multiplication) {
+array<Item> scan_inclusive_no_result(Array<Item>& items, TmpArray<Item>& tmp_array, const Item& identity, const Multiply_fct& multiplication) {
   array<Item> result(items.size() + 1);
-  result[0] = zero;
-  scan_exclusive(items, 0, items.size(), result, 1, tmp_array, 0, zero, multiplication);
+  result[0] = identity;
+  scan_exclusive(items, 0, items.size(), result, 1, tmp_array, 0, identity, multiplication);
   return result;
 }
 
+/*!
+  Calculates the inclusive scan array (i.e. array of prefix sums with identity element first) of given multiplication function on elements
+  of the given array. Uses additional 2 * items.size() / BLOCK_SIZE length temporary array.
+  \param items array of elements
+  \param identity identity element for mulitplication function
+  \param multiplication multiplication function
+  \return array with results of scan
+*/
 template <template <class Item> class Array, class Item, class Multiply_fct>
-array<Item> scan_inclusive(Array<Item>& items, const Item& zero, const Multiply_fct& multiplication) {
+array<Item> scan_inclusive(Array<Item>& items, const Item& identity, const Multiply_fct& multiplication) {
   array<Item> result(items.size() + 1);
-  scan_inclusive_no_tmp(items, result, zero, multiplication);
+  scan_inclusive_no_tmp(items, result, identity, multiplication);
   return result;
 }
 
